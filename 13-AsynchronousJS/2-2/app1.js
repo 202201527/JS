c7f72df4396e85339420c888f374aa0ff138c7df
202201527 <wkddj77@inu.ac.kr>
2024-05-16 15:01:47 +0900

    modified:   13-AsynchronousJS/2-2/app1.js 	modified:   13-AsynchronousJS/2-2/index.html

--- 13-AsynchronousJS/2-2/app1.js
+++ 13-AsynchronousJS/2-2/app1.js
@@ -3,6 +3,7 @@
 fetch("http://localhost:3000/web").then((response) => {
   // When the promise resolves, we have status and headers
   if (response.ok) {
-    console.log(response); // What can we do here? We don't actually have the response body yet.
+    const obj = JSON.parse();
+    console.log(obj); // What can we do here? We don't actually have the response body yet.
   }
 });
